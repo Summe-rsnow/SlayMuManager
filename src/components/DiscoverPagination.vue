e8386eb65623c9a092f53195cd40c9e0ddc99e98
mumu <15432@mumu.dev>
2026-07-31 00:50:35 +0800

    fix: remove unused import in DiscoverPagination.vue

--- src/components/DiscoverPagination.vue
+++ src/components/DiscoverPagination.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed } from "vue"
-import { useI18n } from "vue-i18n"
 import { NIcon, NSelect, NPagination, NInputNumber } from "naive-ui"
 import { List } from "@lucide/vue"
 
